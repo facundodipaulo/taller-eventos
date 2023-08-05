402fea69f53444925a32b66190edd7c107b84aab
facundodipaulo <facundodipaulo7420@gmail.com>
2023-08-05 16:47:18 -0300

    Hola! Soy el div

---
+++ eventos.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const div = document.getElementById("div")
+    const button = document.getElementById("button")
+
+        div.addEventListener("click", ()=> {
+                alert("Hola! Soy el div");
+        })
+})
